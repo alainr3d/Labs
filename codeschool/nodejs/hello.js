ff9cb16924569e3abb055265bfbf6d9721dad325
OpenShift guest <rdilig@gluix.com>
2013-01-04 13:46:58 -0500

    all

---
+++ codeschool/nodejs/hello.js
@@ -0,0 +1,10 @@
+var http = require("http");
+
+http.createServer(function(req, res) { 
+    res.writeHead(200);
+    res.write("Hello, this is dog");
+    res.end();
+    
+}).listen(8080);
+
+console.log('listening on port 8080');
